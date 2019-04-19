ecfc1d0fb667fc0f8a22e098635bb41517875d06
Hearsh Mahidharia <hearsh.m@gmail.com>
2019-04-19 16:35:33 -0400

    Recommender perfected with users and projects

--- components/DataAccess/Recommender/index.js
+++ components/DataAccess/Recommender/index.js
@@ -6,58 +6,44 @@ function Recommender() {
 	this.data = {
 		'tags': null,
 		'users': null,
-	};
+		'tagLength': 0,
+		'userLength': 0,
+	}
+
 }
 
 Recommender.prototype.setRecommender = function(tags, users) {
-	if(this.data.tags === null) {
+	if(!this.data.tags && tags) {
 		this.data.tags = {};
-		for(let i = 0; i < tags.length; i++) {
-			this.data.tags[tags[i]] = {
-				'score': (1 / tags.length),
-				'count': 1,
-			};
-		}
-		this.data['tagLength'] = tags.length;
-		return;
 	}
-	if(this.data.users === null) {
+	if(!this.data.users && users) {
 		this.data.users = {};
-		if(users !== null) {
-			for(let i = 0; i < users.length; i++) {
-				this.data.users[users[i]] = {
-					'score': (1 / users.length),
-					'count': 1,
-				};
-			}
-			this.data['userLength'] = users.length;
-		}
-		return;
 	}
 	this.addTags(tags);
 	this.addUsers(users);
 }
 
 Recommender.prototype.addTags = function(tags) {
-	this.data.tagLength = this.data.tagLength + tags.length;
-	for(let i = 0; i < tags.length; i++) {
-		if(this.data.tags[tags[i]] === undefined) {
-			this.data.tags[tags[i]] = {
-				'score': (1 / this.data.tagLength),
-				'count': 1,
-			}
-		} else {
-			this.data.tags[tags[i]] = {
-				'score': ((this.data.tags[tags[i]].count + 1) / this.data.tagLength),
-				'count': (this.data.tags[tags[i]].count + 1),
+	if(tags) {
+		this.data.tagLength = this.data.tagLength + tags.length;
+		for(let i = 0; i < tags.length; i++) {
+			if(this.data.tags[tags[i]] === undefined) {
+				this.data.tags[tags[i]] = {
+					'score': (1 / this.data.tagLength),
+					'count': 1,
+				}
+			} else {
+				this.data.tags[tags[i]] = {
+					'score': ((this.data.tags[tags[i]].count + 1) / this.data.tagLength),
+					'count': (this.data.tags[tags[i]].count + 1),
+				}
 			}
 		}
 	}
-	return;
 }
 
 Recommender.prototype.addUsers = function(users) {
-	if(users !== null) {
+	if(users) {
 		this.data.userLength = this.data.userLength + users.length;
 		for(let i = 0; i < users.length; i++) {
 			if(this.data.users[users[i]] === undefined) {
@@ -73,7 +59,6 @@ Recommender.prototype.addUsers = function(users) {
 			}
 		}
 	}
-	return;
 }
 
 Recommender.prototype.getRecommendation = function(type) {
@@ -103,63 +88,72 @@ Recommender.prototype.getRecommendation = function(type) {
 	return MaxHeap.getHeap();
 }
 
+Recommender.prototype.fetchProjectData = function(tags) {
+	return new Promise((resolve, reject) => {
+		if(tags === '') {
+			resolve(null);
+		}
+		let url = `http://api.hackaday.io/v1/projects/search?search_term=${tags}&per_page=3&api_key=${config.apiKey}`;
+		fetch(url)
+	  .then(response => response.json())
+	  .then(data => {
+	  	let finalData = {};
+	  	if(data.projects.length) {
+	  		for(let i = 0; i < data.projects.length; i++) {
+	  			if(data.projects[i].name !== undefined) {
+	  				finalData[data.projects[i].id] = {
+			  			'name': data.projects[i].name,
+			  			'link': `projects/${data.projects[i].id}`,
+			  		};
+	  			}
+		  	}
+	  	}
+	  	resolve(finalData);
+	  });
+	});
+}
+
+Recommender.prototype.fetchUserData = function(tags) {
+	return new Promise((resolve, reject) => {
+		if(tags === '') {
+			resolve(null);
+		}
+		let finalData = {};
+		for(let i = 0; i < tags.length; i++) {
+			let url = `http://api.hackaday.io/v1/users/search?tag=${tags[i].id}&per_page=1&api_key=${config.apiKey}`;
+			fetch(url)
+		  .then(response => response.json())
+		  .then(data => {
+		  	if(data.users) {
+		  		finalData[data.users[0].id] = {
+		  			'name': data.users[0].username,
+		  			'link': `${data.users[0].url}`,
+				  };
+		  	}
+		  	if(i === 2) {
+		  		resolve(finalData);
+		  	}
+		  });
+		}
+	});
+}
+
 Recommender.prototype.fetchRecommendation = function(type) {
 	return new Promise((resolve, reject) => {
 		let tags = this.getRecommendation(type);
 		if(tags) {
-			let tagTxt = '';
-			for(let i = 0; i < tags.length; i++) {
-				tagTxt = tagTxt + " " + tags[i].id;
-			}
 			if(type === 'projects') {
-				if(tagTxt === '') {
-					resolve(null);
+				let tagTxt = '';
+				for(let i = 0; i < tags.length; i++) {
+					tagTxt = tagTxt + " " + tags[i].id;
 				}
-				let url = `http://api.hackaday.io/v1/projects/search?search_term=${tagTxt}&per_page=3&api_key=${config.apiKey}`;
-				fetch(url)
-			  .then(response => response.json())
-			  .then(data => {
-			  	let finalData = {};
-			  	if(data.projects.length) {
-			  		for(let i = 0; i < data.projects.length; i++) {
-			  			if(data.projects[i].name !== undefined) {
-			  				finalData[data.projects[i].id] = {
-					  			'name': data.projects[i].name,
-					  			'link': `projects/${data.projects[i].id}`,
-					  		};
-			  			}
-				  	}
-			  	}
-			  	resolve(finalData);
-			  });
+				this.fetchProjectData(tagTxt).then(data => {
+					resolve(data);
+				});
 			}
 			if(type === 'users') {
-				if(tagTxt === '') {
-					resolve(null);
-				}
-				let url = `http://api.hackaday.io/v1/users/search?tag=${tagTxt}&api_key=${config.apiKey}`;
-				fetch(url)
-			  .then(response => response.json())
-			  .then(data => {
-			  	let finalData = {};
-			  	if(data.users && data.users.length) {
-			  		if(data.users.length < 3) {
-			  			for(let i = 0; i < data.projects.length; i++) {
-					  		finalData[data.projects[i].id] = {
-					  			'name': data.users[i].username,
-					  			'link': `${data.users[i].url}`,
-					  		};
-					  	}
-			  		} else {
-			  			for(let i = 0; i < 3; i++) {
-					  		finalData[data.projects[i].id] = {
-					  			'name': data.users[i].username,
-					  			'link': `${data.users[i].url}`,
-					  		};
-					  	}
-			  		}
-			  	}
-			  	resolve(finalData);
+				this.fetchUserData(tags).then(data => {
+					resolve(data);
 				});
 			}
 		} else {
